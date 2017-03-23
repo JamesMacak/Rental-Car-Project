4f8ec79c7f3fb4d1533318fd62da6c91559706e2
JamesMacak <jamesmacak@Jamess-MacBook-Pro.local>
2017-03-23 12:22:40 -0400

    DealerGUI Added Multiple Elements
    Added vehicle options, available vehicles list, wait list, search bar,
    and sales.

--- src/DealerGUI.java
+++ src/DealerGUI.java
@@ -10,10 +10,16 @@ import javax.swing.JSpinner;
 import javax.swing.JComboBox;
 import java.awt.List;
 import javax.swing.border.TitledBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import java.awt.Color;
 
 public class DealerGUI {
 
 	private JFrame frame;
+	private JTextField txtSearch;
 
 	/**
 	 * Launch the application.
@@ -43,7 +49,7 @@ public class DealerGUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 1200, 700);
+		frame.setBounds(100, 100, 958, 700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -82,7 +88,7 @@ public class DealerGUI {
 		
 		JLabel customerName = new JLabel("NULL");
 		customerName.setFont(new Font("Times New Roman", Font.PLAIN, 13));
-		customerName.setBounds(346, 41, 80, 16);
+		customerName.setBounds(346, 41, 226, 16);
 		frame.getContentPane().add(customerName);
 		
 		JLabel lblDateOfBirth = new JLabel("DOB:");
@@ -93,7 +99,7 @@ public class DealerGUI {
 		
 		JLabel customerDateOfBirth = new JLabel("NULL");
 		customerDateOfBirth.setFont(new Font("Times New Roman", Font.PLAIN, 13));
-		customerDateOfBirth.setBounds(346, 69, 80, 16);
+		customerDateOfBirth.setBounds(346, 69, 226, 16);
 		frame.getContentPane().add(customerDateOfBirth);
 		
 		JLabel lblSocialSecurityNumber = new JLabel("SSN:");
@@ -104,7 +110,7 @@ public class DealerGUI {
 		
 		JLabel customerSocialSecurityNumber = new JLabel("NULL");
 		customerSocialSecurityNumber.setFont(new Font("Times New Roman", Font.PLAIN, 13));
-		customerSocialSecurityNumber.setBounds(346, 98, 80, 16);
+		customerSocialSecurityNumber.setBounds(346, 98, 226, 16);
 		frame.getContentPane().add(customerSocialSecurityNumber);
 		
 		JLabel lblAge = new JLabel("Age:");
@@ -115,7 +121,7 @@ public class DealerGUI {
 		
 		JLabel lblCustomerAge = new JLabel("NULL");
 		lblCustomerAge.setFont(new Font("Times New Roman", Font.PLAIN, 13));
-		lblCustomerAge.setBounds(346, 126, 80, 16);
+		lblCustomerAge.setBounds(346, 126, 226, 16);
 		frame.getContentPane().add(lblCustomerAge);
 		
 		JLabel lblGender = new JLabel("Gender:");
@@ -126,7 +132,7 @@ public class DealerGUI {
 		
 		JLabel lblCustomerGender = new JLabel("NULL");
 		lblCustomerGender.setFont(new Font("Times New Roman", Font.PLAIN, 13));
-		lblCustomerGender.setBounds(346, 154, 80, 16);
+		lblCustomerGender.setBounds(346, 154, 226, 16);
 		frame.getContentPane().add(lblCustomerGender);
 		
 		JLabel lblAddress = new JLabel("Address:");
@@ -137,28 +143,94 @@ public class DealerGUI {
 		
 		JLabel lblCustomerAddress1 = new JLabel("NULL");
 		lblCustomerAddress1.setFont(new Font("Times New Roman", Font.PLAIN, 13));
-		lblCustomerAddress1.setBounds(346, 182, 80, 17);
+		lblCustomerAddress1.setBounds(346, 182, 226, 17);
 		frame.getContentPane().add(lblCustomerAddress1);
 		
 		JLabel lblCustomerAddress2 = new JLabel("NULL");
 		lblCustomerAddress2.setFont(new Font("Times New Roman", Font.PLAIN, 13));
-		lblCustomerAddress2.setBounds(346, 200, 80, 16);
+		lblCustomerAddress2.setBounds(346, 200, 226, 16);
 		frame.getContentPane().add(lblCustomerAddress2);
 		
 		JLabel lblCustomerAddress3 = new JLabel("NULL");
 		lblCustomerAddress3.setFont(new Font("Times New Roman", Font.PLAIN, 13));
-		lblCustomerAddress3.setBounds(346, 218, 81, 16);
+		lblCustomerAddress3.setBounds(346, 218, 226, 16);
 		frame.getContentPane().add(lblCustomerAddress3);
 		
 		JPanel VehicleMenu = new JPanel();
 		VehicleMenu.setBorder(new TitledBorder(null, "JPanel title", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		VehicleMenu.setBounds(273, 266, 240, 200);
+		VehicleMenu.setBounds(273, 266, 299, 203);
 		frame.getContentPane().add(VehicleMenu);
 		VehicleMenu.setLayout(null);
 		
 		JLabel lblErrorMessage = new JLabel("SOME ERROR MESSAGE");
+		lblErrorMessage.setHorizontalAlignment(SwingConstants.CENTER);
 		lblErrorMessage.setFont(new Font("Times New Roman", Font.PLAIN, 13));
-		lblErrorMessage.setBounds(6, 18, 228, 16);
+		lblErrorMessage.setBounds(6, 18, 287, 16);
 		VehicleMenu.add(lblErrorMessage);
+		
+		JButton btnCar = new JButton("Car");
+		btnCar.setFont(new Font("Times New Roman", Font.PLAIN, 28));
+		btnCar.setBounds(6, 46, 90, 90);
+		VehicleMenu.add(btnCar);
+		
+		JButton btnVan = new JButton("Van");
+		btnVan.setFont(new Font("Times New Roman", Font.PLAIN, 28));
+		btnVan.setBounds(104, 46, 90, 90);
+		VehicleMenu.add(btnVan);
+		
+		JButton btnTruck = new JButton("Truck");
+		btnTruck.setFont(new Font("Times New Roman", Font.PLAIN, 28));
+		btnTruck.setBounds(203, 46, 90, 90);
+		VehicleMenu.add(btnTruck);
+		
+		JButton btnNewButton = new JButton("Add");
+		btnNewButton.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		btnNewButton.setBounds(26, 148, 117, 38);
+		VehicleMenu.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Wait");
+		btnNewButton_1.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(155, 148, 117, 38);
+		VehicleMenu.add(btnNewButton_1);
+		
+		List avaliableVehicles = new List();
+		avaliableVehicles.setBounds(601, 45, 348, 291);
+		frame.getContentPane().add(avaliableVehicles);
+		
+		List waitList = new List();
+		waitList.setBounds(601, 377, 348, 291);
+		frame.getContentPane().add(waitList);
+		
+		JLabel lblAvailableVehicles = new JLabel("Available Vehicles");
+		lblAvailableVehicles.setFont(new Font("Times New Roman", Font.PLAIN, 28));
+		lblAvailableVehicles.setBounds(601, 13, 348, 29);
+		frame.getContentPane().add(lblAvailableVehicles);
+		
+		JLabel lblWaitList = new JLabel("Wait List");
+		lblWaitList.setFont(new Font("Times New Roman", Font.PLAIN, 28));
+		lblWaitList.setBounds(601, 342, 348, 29);
+		frame.getContentPane().add(lblWaitList);
+		
+		txtSearch = new JTextField();
+		txtSearch.setForeground(Color.LIGHT_GRAY);
+		txtSearch.setHorizontalAlignment(SwingConstants.CENTER);
+		txtSearch.setFont(new Font("Times New Roman", Font.PLAIN, 24));
+		txtSearch.setText("Search");
+		txtSearch.setBounds(4, 516, 240, 39);
+		frame.getContentPane().add(txtSearch);
+		txtSearch.setColumns(10);
+		
+		JLabel lblSales = new JLabel("Sales");
+		lblSales.setFont(new Font("Times New Roman", Font.PLAIN, 24));
+		lblSales.setBounds(273, 481, 299, 29);
+		frame.getContentPane().add(lblSales);
+		
+		List list = new List();
+		list.setBounds(273, 522, 299, 146);
+		frame.getContentPane().add(list);
 	}
 }
